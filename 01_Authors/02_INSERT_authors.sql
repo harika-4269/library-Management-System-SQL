e2d6fe2e37792d4e9ff4f752b8853a56af13bdc9
harika-4269 <harikaqueen1@gmail.com>
2026-08-29 14:05:22 +0530

    Create 02_INSERT_authors.sql

---
+++ 01_Authors/02_INSERT_authors.sql
@@ -0,0 +1,8 @@
+-- Insert sample authors
+INSERT INTO authors (author_id, name, nationality, birth_year) VALUES
+(1, 'Niharika',   'Indian',   1965),
+(2, 'Balu',  'British',   1903),
+(3, 'Sai',  'Nigerian',  1930),
+(4, 'Tejasri','Japanese',  1949),
+(5, 'Niha','British',   1890),
+(6, 'Ram', 'Nigerian', 1977);
