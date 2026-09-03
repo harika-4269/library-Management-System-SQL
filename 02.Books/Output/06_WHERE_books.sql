97adbbbc3a27d2308822c45fdb2b5a196d6e7da5
harika-4269 <harikaqueen1@gmail.com>
2026-09-03 11:58:34 +0530

    Create 06_WHERE_books.sql

---
+++ 02.Books/Output/06_WHERE_books.sql
@@ -0,0 +1,14 @@
+/*
+-------------------------------
+OUTPUT:
+-------------------------------
+-- Books priced above 400:
+book_id | title                                     | price
+1       | Harry Potter and the Philosopher's Stone   | 699.00
+4       | Norwegian Wood                             | 450.00
+
+-- Books in category 'Fiction':
+book_id | title              | category
+3       | Things Fall Apart  | Fiction
+4       | Norwegian Wood     | Fiction
+*/
