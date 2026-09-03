8d9d8fb9777f5709b1e73678e2a5134f1cae14b5
harika-4269 <harikaqueen1@gmail.com>
2026-09-03 13:00:38 +0530

    Create 06_WHERE_transactions.sql

---
+++ 04_Transactions/Output/06_WHERE_transactions.sql
@@ -0,0 +1,14 @@
+/*
+-------------------------------
+OUTPUT:
+-------------------------------
+-- Issued after 2024-02-08:
+transaction_id | book_id | member_id | issue_date
+3               | 4       | 3         | 2024-02-10
+5               | 5       | 4         | 2024-02-18
+
+-- For member_id = 1:
+transaction_id | book_id | issue_date | return_date | status
+1               | 1       | 2024-02-01 | 2024-02-15  | RETURNED
+4               | 3       | 2024-02-12 | 2024-02-20  | RETURNED
+*/
