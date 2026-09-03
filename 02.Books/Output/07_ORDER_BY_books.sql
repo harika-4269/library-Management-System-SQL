b653e9e6a74eb533cad868743e5b01e5066f30c5
harika-4269 <harikaqueen1@gmail.com>
2026-09-03 11:59:13 +0530

    Create 07_ORDER_BY_books.sql

---
+++ 02.Books/Output/07_ORDER_BY_books.sql
@@ -0,0 +1,16 @@
+/*
+-------------------------------
+OUTPUT:
+-------------------------------
+-- Ordered by price DESC:
+Harry Potter and the Philosopher's Stone -- 699.00
+Norwegian Wood                           -- 450.00
+1984                                     -- 399.00
+Things Fall Apart                        -- 349.00
+Murder on the Orient Express              -- 299.00
+Animal Farm                              -- 250.00
+
+-- Ordered alphabetically:
+1984, Animal Farm, Harry Potter and the Philosopher's Stone,
+Murder on the Orient Express, Norwegian Wood, Things Fall Apart
+*/
