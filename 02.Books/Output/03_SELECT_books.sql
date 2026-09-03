f86a1067bd1fe91e98df2450538b26b48e7e1557
harika-4269 <harikaqueen1@gmail.com>
2026-09-03 11:56:14 +0530

    Create 03_SELECT_books.sql

---
+++ 02.Books/Output/03_SELECT_books.sql
@@ -0,0 +1,14 @@
+/*
+-------------------------------
+OUTPUT:
+-------------------------------
+book_id | title                                     | price
+1       | Harry Potter and the Philosopher's Stone   | 699.00
+2       | 1984                                       | 399.00
+3       | Things Fall Apart                          | 349.00
+4       | Norwegian Wood                             | 450.00
+5       | Murder on the Orient Express                | 299.00
+6       | Animal Farm                                | 250.00
+
+6 rows in set.
+*/
