817e271cae866b2a73855dbed4cc08c75f3ba882
harika-4269 <harikaqueen1@gmail.com>
2026-09-03 11:57:34 +0530

    Create 04_UPDATE_books.sql

---
+++ 02.Books/Output/04_UPDATE_books.sql
@@ -0,0 +1,8 @@
+/*
+-------------------------------
+OUTPUT:
+-------------------------------
+Query OK, 1 row affected.
+Rows matched: 1  Changed: 1  Warnings: 0
+-- book_id 2 (1984) price changed to 420.00
+*/
