d28d239bd50da628630387adcc7b22be2f308c2b
harika-4269 <harikaqueen1@gmail.com>
2026-09-03 11:45:34 +0530

    Create 02_INSERT_books.sql

---
+++ 02.Books/02_INSERT_books.sql
@@ -0,0 +1,8 @@
+-- Insert sample books
+INSERT INTO books (book_id, title, author_id, category, price, total_copies, available_copies) VALUES
+(1, 'Harry Potter and the Philosopher''s Stone', 1, 'Fantasy',   699.00, 5, 3),
+(2, '1984',                                      2, 'Dystopian', 399.00, 4, 2),
+(3, 'Things Fall Apart',                         3, 'Fiction',   349.00, 3, 3),
+(4, 'Norwegian Wood',                            4, 'Fiction',   450.00, 2, 0),
+(5, 'Murder on the Orient Express',              5, 'Mystery',   299.00, 6, 4),
+(6, 'Animal Farm',                               2, 'Dystopian', 250.00, 4, 4);
