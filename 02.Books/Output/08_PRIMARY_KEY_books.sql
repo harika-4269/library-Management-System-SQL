e32c7dd0e9c938c6ab87aef2141dec3e8b7630ff
harika-4269 <harikaqueen1@gmail.com>
2026-09-03 12:00:09 +0530

    Create 08_PRIMARY_KEY_books.sql

---
+++ 02.Books/Output/08_PRIMARY_KEY_books.sql
@@ -0,0 +1,8 @@
+/*
+-------------------------------
+OUTPUT:
+-------------------------------
+Query OK, 0 rows affected.
+Records: 6  Duplicates: 0  Warnings: 0
+-- Confirms book_id is unique and NOT NULL for every row
+*/
