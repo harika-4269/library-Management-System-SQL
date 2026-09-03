06d29a239a46938fd24c47fb3d0aad535dc8232f
harika-4269 <harikaqueen1@gmail.com>
2026-09-03 12:56:17 +0530

    Create 03_SELECT_transactions.sql

---
+++ 04_Transactions/Output/03_SELECT_transactions.sql
@@ -0,0 +1,10 @@
+/*
+-------------------------------
+OUTPUT:
+-------------------------------
+-- Currently issued transactions:
+transaction_id | book_id | member_id | issue_date | status
+2               | 2       | 2         | 2024-02-05 | ISSUED
+3               | 4       | 3         | 2024-02-10 | ISSUED
+5               | 5       | 4         | 2024-02-18 | ISSUED
+*/
