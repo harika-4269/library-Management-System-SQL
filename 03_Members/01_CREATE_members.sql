7d67f1e33041ba78018162d4f26fd75f6bb50a71
harika-4269 <harikaqueen1@gmail.com>
2026-09-03 12:21:15 +0530

    Create 01_CREATE_members.sql

---
+++ 03_Members/01_CREATE_members.sql
@@ -0,0 +1,8 @@
+-- Create the Members table
+CREATE TABLE members (
+    member_id     INT PRIMARY KEY AUTO_INCREMENT,
+    name          VARCHAR(100) NOT NULL,
+    email         VARCHAR(100) UNIQUE,
+    phone         VARCHAR(15),
+    join_date     DATE
+);
