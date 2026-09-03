b50250b3ea6c21434e16e55039fa5d3a99daa19e
harika-4269 <harikaqueen1@gmail.com>
2026-09-03 12:39:56 +0530

    Create 04_UPDATE_members.sql

---
+++ 03_Members/Outputs/04_UPDATE_members.sql
@@ -0,0 +1,8 @@
+/*
+-------------------------------
+OUTPUT:
+-------------------------------
+Query OK, 1 row affected.
+Rows matched: 1  Changed: 1  Warnings: 0
+-- member_id 3 (Arjun Patel) phone changed to '9998887771'
+*/
