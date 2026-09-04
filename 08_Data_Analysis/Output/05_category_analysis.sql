6936574b24f97cd0d1e9fdc662ad02d63828e722
harika-4269 <harikaqueen1@gmail.com>
2026-09-04 12:47:37 +0530

    Create 05_category_analysis.sql

---
+++ 08_Data_Analysis/Output/05_category_analysis.sql
@@ -0,0 +1,10 @@
+/*
+-------------------------------
+OUTPUT:
+-------------------------------
+category  | avg_price | total_available
+Fantasy   | 699.00    | 3
+Dystopian | 324.50    | 6
+Fiction   | 399.50    | 3
+Mystery   | 299.00    | 4
+*/
