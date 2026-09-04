244ccbcb32a150e32cf1634be6eb31bb1a41ba1e
harika-4269 <harikaqueen1@gmail.com>
2026-09-04 12:24:30 +0530

    Create 03_member_activity.sql

---
+++ 08_Data_Analysis/03_member_activity.sql
@@ -0,0 +1,6 @@
+-- How many books each member currently has issued (not yet returned)
+SELECT m.name AS member_name, COUNT(t.transaction_id) AS currently_issued
+FROM members m
+JOIN transactions t ON m.member_id = t.member_id
+WHERE t.status = 'ISSUED'
+GROUP BY m.name;
