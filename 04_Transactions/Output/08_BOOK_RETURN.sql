4fe3f889db9661c5d5e5af8d7ba6f05483d4987d
harika-4269 <harikaqueen1@gmail.com>
2026-09-04 11:17:58 +0530

    Create 08_BOOK_RETURN.sql

---
+++ 04_Transactions/Output/08_BOOK_RETURN.sql
@@ -0,0 +1,7 @@
+/*
+-------------------------------
+OUTPUT:
+-------------------------------
+Step 1 -> Query OK, 1 row affected. (transaction marked RETURNED with today's date)
+Step 2 -> Query OK, 1 row affected. (books.available_copies increased by 1)
+*/
