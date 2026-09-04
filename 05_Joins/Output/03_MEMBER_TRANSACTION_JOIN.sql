e9a728f316597b889ed095971d7fca96cc2b1cbf
harika-4269 <harikaqueen1@gmail.com>
2026-09-04 11:37:36 +0530

    Create 03_MEMBER_TRANSACTION_JOIN.sql

---
+++ 05_Joins/Output/03_MEMBER_TRANSACTION_JOIN.sql
@@ -0,0 +1,11 @@
+/*
+-------------------------------
+OUTPUT:
+-------------------------------
+transaction_id | member_name  | issue_date | status
+1               | Ravi Kumar   | 2024-02-01 | RETURNED
+2               | Sneha Reddy  | 2024-02-05 | ISSUED
+3               | Arjun Patel  | 2024-02-10 | ISSUED
+4               | Ravi Kumar   | 2024-02-12 | RETURNED
+5               | Priya Sharma | 2024-02-18 | ISSUED
+*/                                         
