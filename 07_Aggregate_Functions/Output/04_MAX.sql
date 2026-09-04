2ace0f905ef226c567e8346d99779feae7c16dae
harika-4269 <harikaqueen1@gmail.com>
2026-09-04 12:20:58 +0530

    Create 04_MAX.sql

---
+++ 07_Aggregate_Functions/Output/04_MAX.sql
@@ -0,0 +1,7 @@
+/*
+-------------------------------
+OUTPUT:
+-------------------------------
+title                                      | price
+Harry Potter and the Philosopher's Stone   | 699.00
+*/
