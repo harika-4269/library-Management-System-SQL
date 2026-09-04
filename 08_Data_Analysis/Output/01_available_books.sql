b8ccca83ce150a00f2be26d50ebde2d6dc611514
harika-4269 <harikaqueen1@gmail.com>
2026-09-04 12:26:27 +0530

    Create 01_available_books.sql

---
+++ 08_Data_Analysis/Output/01_available_books.sql
@@ -0,0 +1,11 @@
+/*
+-------------------------------
+OUTPUT:
+-------------------------------
+title                                       | available_copies
+Harry Potter and the Philosopher's Stone    | 3
+1984                                        | 2
+Things Fall Apart                           | 3
+Murder on the Orient Express                | 4
+Animal Farm                                 | 4
+*/
