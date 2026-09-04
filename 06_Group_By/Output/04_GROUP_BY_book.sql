e9e7ccd28675ca78a774c542ecefc2ca7e6893aa
harika-4269 <harikaqueen1@gmail.com>
2026-09-04 12:10:38 +0530

    Create 04_GROUP_BY_book.sql

---
+++ 06_Group_By/Output/04_GROUP_BY_book.sql
@@ -0,0 +1,11 @@
+/*
+-------------------------------
+OUTPUT:
+-------------------------------
+title                                      | times_issued
+Harry Potter and the Philosopher's Stone   | 1
+1984                                       | 1
+Norwegian Wood                             | 1
+Things Fall Apart                          | 1
+Murder on the Orient Express               | 1
+*/
