8cccd991457abfccc9f9440aba7476bf323a89cb
harika-4269 <harikaqueen1@gmail.com>
2026-09-04 11:17:26 +0530

    Create 07_BOOK_ISSUE.sql

---
+++ 04_Transactions/Output/07_BOOK_ISSUE.sql
@@ -0,0 +1,7 @@
+/*
+-------------------------------
+OUTPUT:
+-------------------------------
+Step 1 -> Query OK, 1 row affected. (new transaction inserted, status = ISSUED)
+Step 2 -> Query OK, 1 row affected. (books.available_copies reduced by 1)
+*/
