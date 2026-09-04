d691241b3ec712e7050858d15b20c2d1d8b47780
harika-4269 <harikaqueen1@gmail.com>
2026-09-04 11:43:38 +0530

    Create 04_LIBRARY_FULL_JOIN.sql

---
+++ 05_Joins/Output/04_LIBRARY_FULL_JOIN.sql
@@ -0,0 +1,11 @@
+/*
+-------------------------------
+OUTPUT:
+-------------------------------
+member_name  | book_title                                 | author_name     | issue_date | status
+Ravi Kumar   | Harry Potter and the Philosopher's Stone   | Niharika        | 2024-02-01 | RETURNED
+Sneha Reddy  | 1984                                       | Balu            | 2024-02-05 | ISSUED
+Arjun Patel  | Things Fall Apart                          | Sai             | 2024-02-10 | ISSUED
+Ravi Kumar   | Norwegian Wood                             | Tejasri         | 2024-02-12 | RETURNED
+Priya Sharma | Murder on the Orient Express               | Ram             | 2024-02-18 | ISSUED
+*/
