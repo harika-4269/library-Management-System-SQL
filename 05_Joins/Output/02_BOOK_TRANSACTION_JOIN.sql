ac0a5352ff88120cb7ccac7e36953ce29016113a
harika-4269 <harikaqueen1@gmail.com>
2026-09-04 11:34:56 +0530

    Create 02_BOOK_TRANSACTION_JOIN.sql

---
+++ 05_Joins/Output/02_BOOK_TRANSACTION_JOIN.sql
@@ -0,0 +1,11 @@
+/*
+-------------------------------
+OUTPUT:
+-------------------------------
+transaction_id | title                                     | issue_date | status
+1               | Harry Potter and the Philosopher's Stone   | 2024-02-01 | RETURNED
+2               | 1984                                       | 2024-02-05 | ISSUED
+3               | Norwegian Wood                             | 2024-02-10 | ISSUED
+4               | Things Fall Apart                          | 2024-02-12 | RETURNED
+5               | Murder on the Orient Express                | 2024-02-18 | ISSUED
+*/
