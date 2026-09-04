81936360b05e8d90e704a47e604d7392be660b76
harika-4269 <harikaqueen1@gmail.com>
2026-09-04 12:08:49 +0530

    Create 03_GROUP_BY_member.sql

---
+++ 06_Group_By/Output/03_GROUP_BY_member.sql
@@ -0,0 +1,10 @@
+/*
+-------------------------------
+OUTPUT:
+-------------------------------
+member_name  | total_borrowed
+Ravi Kumar   | 2
+Sneha Reddy  | 1
+Arjun Patel  | 1
+Priya Sharma | 1
+*/
