3badb2e4870ed6d6461e3b4f654dc58de0dd5778
harika-4269 <harikaqueen1@gmail.com>
2026-09-04 12:05:52 +0530

    Create 02_GROUP_BY_author.sql

---
+++ 06_Group_By/Output/02_GROUP_BY_author.sql
@@ -0,0 +1,11 @@
+/*
+-------------------------------
+OUTPUT:
+-------------------------------
+author_name      | total_books
+Niharika         | 1
+Balu             | 2
+Sai              | 1
+Tejasri          | 1
+Niha             | 1
+*/
